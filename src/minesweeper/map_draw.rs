988eb836436defbd2559bbb588497c90a71382cd
Bali <bali@geosys.nl>
2024-09-12 17:59:05 +0200

    Fix question mark in the reveal phase

--- src/minesweeper/map_draw.rs
+++ src/minesweeper/map_draw.rs
@@ -150,7 +150,7 @@ fn generate_line(mine_line: &Vec<TileState>, mine_char: char, show_revealed: boo
             TileState::Marked(num) => line.push(if mine_char == ' ' {'.'} else { if *num < 0 {mine_char} else { 'M' }}),
             TileState::HiddenEmpty(_) => line.push(' '),
             TileState::VisibleEmpty(num) => if show_revealed { line.push_str(num.to_string().as_str()) } else {line.push(' ')},
-            TileState::Question(_) => line.push('?'),
+            TileState::Question(num) => line.push(if mine_char == ' ' {'?'} else { if *num < 0 {mine_char} else { '?' }}),
         }
         line.push('|');
         for _ in 0..spaces {
