55792dc54cb8d0ec7df398611e6ab0c68ffcd0a9
Bali <bali@geosys.nl>
2025-06-04 14:59:19 +0200

    Fix linux build + lowercase package name

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
+#[cfg(target_os = "windows")]
 fn main() {
     println!("cargo:rerun-if-changed=src/minesweeper.rc");
-    // Compile and link checksums.rc
+    // Compile and link windows resources
     embed_resource::compile("src/minesweeper.rc", embed_resource::NONE).manifest_optional().unwrap();
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {
 }
